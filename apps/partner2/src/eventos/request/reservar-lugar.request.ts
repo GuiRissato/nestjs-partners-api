e2a51b733dfb2eb894f1f228eb75c45d550d4d93
GuiRissato <guilherme_andrade_rissato@hotmail.com>
2024-06-21 23:26:59 -0300

    desafio

--- apps/partner2/src/eventos/request/reservar-lugar.request.ts
+++
@@ -1,7 +0,0 @@
-import { TicketKind } from '@prisma/client';
-
-export class ReservarLugarRequest {
-  lugares: string[];
-  tipo_ingresso: TicketKind;
-  email: string;
-}
--- apps/partner2/src/lugares/spots.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { LugaresController } from './spots.controller';
-import { SpotsCoreModule } from '@app/core/spots/spots-core.module';
-
-@Module({
-  imports: [SpotsCoreModule],
-  controllers: [LugaresController],
-})
-export class LugaresModule {}
